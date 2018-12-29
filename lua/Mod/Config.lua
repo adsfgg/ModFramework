3d74c0ebf0b68cfd057b5942a14dbae3052d31d9
adsfgg <11364579+adsfgg@users.noreply.github.com>
2018-12-29 15:41:37 +0000

    initial commit

---
+++ lua/Mod/Config.lua
@@ -0,0 +1,8 @@
+kAllowModDebugMessages = true
+kShowModInFeedbackText = false
+kModVersion = "0"
+kModBuild = "1"
+
+Modules = 
+{
+}
---
+++ lua/Mod/Framework/Mod_Client.lua
@@ -0,0 +1,15 @@
+ModPrintDebug("Loading Client files", "Client")
+
+for i = 1, #Modules do
+	path = FormatDir(Modules[i], "Client")
+
+	local ClientFiles = {}
+	Shared.GetMatchingFileNames(path, true, ClientFiles)
+
+	for i = 1, #ClientFiles do
+		ModPrintDebug("Loading client file: " .. ClientFiles[i], "Client")
+	    Script.Load(ClientFiles[i])
+	end
+end
+
+ModPrintDebug("Client files loaded.", "Client")
