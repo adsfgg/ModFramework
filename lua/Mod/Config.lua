e9d4697b71c6e9e42cadfbbbc0f9cd06628ec6c6
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-06 18:21:30 +0000

    add backend changes. more validation more fun

--- lua/Mod/Config.lua
+++ lua/Mod/Config.lua
@@ -1,11 +1,14 @@
--- i wonder if this will still work with multiple mods using the framework
--- prolly not but hey :)
+function GetModConfig(kLogLevels)
+	local config = {}
 
-Mod.config.kLogLevel = 4
-Mod.config.kShowInFeedbackText = true
-Mod.config.kModVersion = "0"
-Mod.config.kModBuild = "1"
+	config.kLogLevel = kLogLevels.debug
+	config.kShowInFeedbackText = true
+	config.kModVersion = "0"
+	config.kModBuild = "1"
 
-Mod.config.modules =
-{
-}
+	config.modules =
+	{
+	}
+
+	return config
+end
