596fc72e5282f6cadce8d94e834e680001718a17
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-24 17:11:56 +0000

    make bindings use events instead of whatever was happening before

--- lua/ModFramework/Framework/Framework.lua
+++ lua/ModFramework/Framework/Framework.lua
@@ -995,4 +995,38 @@ function Mod:TableLength(tbl)
   return count
 end
 
+--[[
+========================
+      Binding Funcs
+========================
+]]
+local function UpdateBindingData()
+  local globalControlBindings = Mod:GetLocalVariable(BindingsUI_GetBindingsData, "globalControlBindings")
+  local bindingChanges = Mod:GetBindingAdditions()
+
+  for _,v in ipairs(bindingChanges) do
+    local afterName = v[5]
+
+    Mod:PrintDebug("Adding new bind \"" .. v[1].. "\" after " .. afterName)
+
+    v[3] = Locale.ResolveString(v[3])
+
+    local index
+
+    for i,v in ipairs(globalControlBindings) do
+      if v == afterName then
+        index = i + 4
+      end
+    end
+
+    assert(index, "BindingChanges: Binding \"" .. afterName .. "\" does not exist.")
+
+    for i=0,3 do
+      table.insert(globalControlBindings, index + i, v[i + 1])
+    end
+  end
+end
+
+Event.Hook("LoadComplete", UpdateBindingData)
+
 Mod:Initialise()
