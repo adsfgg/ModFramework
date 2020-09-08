6ae448521e695cdd8d6ddb52864a666cc4511471
adsfgg <11364579+adsfgg@users.noreply.github.com>
2020-09-08 17:28:02 +0100

    Fix debugprint call in TechTreeConstants; Fix stupid bug in EnumUtilitiesModule

--- src/lua/ModFramework/Files/techhandler/Post/TechTreeConstants.lua
+++ src/lua/ModFramework/Files/techhandler/Post/TechTreeConstants.lua
@@ -1,12 +1,12 @@
-local mod = %__MODNAME__%
+local mod = BalanceMod
 local techHandler = mod:GetModule('techhandler')
 local enumUtils = mod:GetModule('enumutilities')
 local logger = mod:GetModule('logger')
 
 local newTechNames = techHandler:GetTechIdsToAdd()
 
-fw_print_debug(techHandler, "Adding new TechIds")
+logger:PrintDebug("Adding new TechIds")
 for _,v in ipairs(newTechNames) do
-    fw_print_debug(techHandler, "Adding: %s", v)
     enumUtils:AppendToEnum(kTechId, v)
+    logger:PrintDebug("Adding: %s(%s)", v, kTechId[v])
 end
