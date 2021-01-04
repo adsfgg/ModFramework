d634cfc0e8e4b1806e37e61e1724e2722d7b906b
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-04 14:55:15 +0000

    Update revision feedback text

--- src/lua/ModFramework/ModFramework.lua
+++ src/lua/ModFramework/ModFramework.lua
@@ -10,7 +10,7 @@ Script.Load("lua/%__MODNAME__%/ModFramework/Modules/TechHandlerModule.lua")
 
 class 'ModFramework'
 
-local modFrameworkVersion = "1.1.3"
+local modFrameworkVersion = "1.1.4"
 
 function ModFramework:Initialize(vm, filehook)
     self.modName = "%__MODNAME__%"
